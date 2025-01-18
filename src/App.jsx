bf44b0eec1b3006e839cab0ccf4e0edddf99c1fa
Federico <fedealtava1992@gmail.com>
2025-01-18 18:40:44 +0100

    split fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,21 @@ const CAT_PREFIX_IMAGE_URL = 'http://cataas.com'
 export function App () {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
+
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then(res => res.json())
       .then(data => {
         const { fact } = data
         setFact(fact)
-        const threeFirstWord = fact.split('', 3).join(' ')
+        const threeFirstWord = fact.split(' ', 3).join(' ')
 
         fetch(`http://cataas.com/cat/says/${threeFirstWord}?size=50&color=red`)
           .then(res => res.json())
           .then(response => {
             const { url } = response
             setImageUrl(url)
-        })
+          })
       })
       .catch(err => console.error(err))
   }, [])
